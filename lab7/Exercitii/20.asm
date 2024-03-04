4324e2a5b7b9196cba9b9a786874afb437b5a389
Razvan2107 <123764280+Razvan2107@users.noreply.github.com>
2024-03-05 00:37:33 +0200

    Add files via upload

---
+++ lab7/Exercitii/20.asm
@@ -0,0 +1,43 @@
+bits 32 
+
+global start        
+
+extern exit               
+import exit msvcrt.dll    
+
+segment data use32 class=data
+    s db 'Ana are un cojoc'
+    len equ $-s
+    max_cuv equ 50
+    cuv resb max_cuv
+segment code use32 class=code
+    start:
+        ; 1. separam cuvintele
+        mov ecx, len
+        cld
+        mov esi,s
+    urmator:
+        mov ebx, 0      ; numar de litere din fiecare cuvant
+        mov edi,cuv
+    repeta:
+        ; citesc s[i]
+        lodsb       ; AL='A' AL <- <DS:ESI> si inc esi
+
+        ; verific daca e spatiu
+        cmp al,' '
+        je e_spatiu
+        
+        ; e caracter
+        stosb       ; <ES:EDI> <- AL si inc EDI
+        inc ebx
+        loop repeta
+    e_spatiu:
+        dec ecx
+        ; verific ca e palindrom
+        
+        ; trec la urmatorul cuvant
+        cmp ecx, 0
+        jnz urmator
+        
+        push    dword 0      
+        call    [exit]       
